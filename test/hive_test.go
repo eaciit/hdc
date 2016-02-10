c0507a3717c6700d2cc108bd0351ba1e65e66590
frezadev <freza.dev@gmail.com>
2016-02-10 11:42:34 +0700

    changes

--- test/hive_test.go
+++ test/hive_test.go
@@ -11,8 +11,29 @@ import (
 func TestHiveConnect(t *testing.T) {
 	cmdStr := "beeline -u jdbc:hive2://192.168.0.223:10000/default -n developer -p b1gD@T@ -e \"select * from sample_07 limit 10;\""
 	cmd := exec.Command("sh", "-c", cmdStr)
-	out, err := cmd.StdoutPipe()
+	/*out, err := cmd.Output()
 	log.Printf("cmd: %s\n", cmd)
 	log.Printf("out: %s\n", out)
-	log.Printf("result: %v\n", err.Error())
+	log.Printf("result: %v\n", err.Error())*/
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	/*var person struct {
+		Name string
+		Age  int
+	}
+	if err := json.NewDecoder(stdout).Decode(&person); err != nil {
+		log.Fatal(err)
+	}*/
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("out: %s\n", stdout)
+	//fmt.Printf("%s is %d years old\n", person.Name, person.Age)
 }
