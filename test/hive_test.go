32332513d24bdfb3a5560738e0cd4c3c904efa48
frezadev <freza.dev@gmail.com>
2016-02-10 11:22:32 +0700

    test the hive using exec command from go

--- test/hive_test.go
+++ test/hive_test.go
@@ -2,8 +2,17 @@ package hive_test
 
 import (
 	"testing"
+	"os/exec"
+	"log"
+	/*"fmt"
+	"os"*/
 )
 
 func TestHiveConnect(t *testing.T) {
-
+	cmdStr := "beeline -u jdbc:hive2://192.168.0.223:10000/default -n developer -p b1gD@T@ -e \"select * from sample_07 limit 10;\""
+	cmd := exec.Command("sh", "-c", cmdStr)
+	out, err := cmd.StdoutPipe()
+	log.Printf("cmd: %s\n", cmd)
+	log.Printf("out: %s\n", out)
+	log.Printf("result: %v\n", err.Error())
 }
