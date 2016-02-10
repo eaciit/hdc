d759079a42c67aa7d0e9cc85368a639d73129ec5
frezadev <freza.dev@gmail.com>
2016-02-10 16:46:37 +0700

    changes

--- hive/hive.go
+++ hive/hive.go
@@ -1,6 +1,9 @@
 package hive
 
 import (
+	"fmt"
+	// "log"
+	"os/exec"
 	"os/user"
 )
 
@@ -10,13 +13,20 @@ type Hive struct {
 	Server      string
 	User        string
 	Password    string
+	DBName      string
 	HiveCommand string
 }
 
-func NewHiveConfig(server, userid, password string) *Hive {
+func HiveConfig(server, dbName, userid, password string) *Hive {
 	hv := Hive{}
 	hv.Server = server
 	hv.Password = password
+
+	if dbName == "" {
+		dbName = "default"
+	}
+
+	hv.DBName = dbName
 
 	if userid == "" {
 		user, err := user.Current()
@@ -26,17 +36,29 @@ func NewHiveConfig(server, userid, password string) *Hive {
 	}
 
 	hv.User = userid
+
 	return &hv
 }
 
 func (h *Hive) Connect() error {
+	cmdStr := "beeline -u jdbc:hive2://" + h.Server + "/" + h.DBName + " -n " + h.User + " -p " + h.Password
+	cmd := exec.Command("sh", "-c", cmdStr)
+	out, err := cmd.Output()
+	_ = out
+	_ = err
 	return nil
 }
 
-func (h *Hive) Exec(query *string, fn FnHiveReceive) (hs *HiveSession, e error) {
-	return
+func (h *Hive) Exec(query string, fn FnHiveReceive) (hs *HiveSession, e error) {
+	cmdStr := "beeline -u jdbc:hive2://" + h.Server + "/" + h.DBName + " -n " + h.User + " -p " + h.Password + "-e" + "\"" + query + "\""
+	cmd := exec.Command("sh", "-c", cmdStr)
+	out, err := cmd.Output()
+	fmt.Printf("result: %s\n", out)
+	_ = err
+	return nil, nil
 }
 
-func (h *Hive) ExecFile(filepath *string, fn FnHiveReceive) (hs *HiveSession, e error) {
-	return
+func (h *Hive) ExecFile(filepath string, fn FnHiveReceive) (hs *HiveSession, e error) {
+
+	return nil, nil
 }
--- test/hive_test.go
+++ test/hive_test.go
@@ -1,18 +1,44 @@
 package hive_test
 
 import (
+	. "github.com/frezadev/hdc/hive"
+	//"os/exec"
 	"testing"
-	"os/exec"
-	"log"
-	/*"fmt"
-	"os"*/
+	//"fmt"
+	"os"
 )
 
-func TestHiveConnect(t *testing.T) {
+/*func TestHiveConnect(t *testing.T) {
 	cmdStr := "beeline -u jdbc:hive2://192.168.0.223:10000/default -n developer -p b1gD@T@ -e \"select * from sample_07 limit 10;\""
 	cmd := exec.Command("sh", "-c", cmdStr)
 	out, err := cmd.Output()
 	log.Printf("cmd: %s\n", cmd)
 	log.Printf("out: %s\n", out)
 	log.Printf("result: %v\n", err.Error())
+}*/
+
+//var e error
+var fnHR FnHiveReceive
+
+//var hSess HiveSession
+var h *Hive
+
+func killApp(code int) {
+	os.Exit(code)
 }
+
+func TestHiveConnect(t *testing.T) {
+	h = HiveConfig("192.168.0.223:10000", "default", "developer", "b1gD@T@")
+}
+
+func TestHiveExec(t *testing.T) {
+	q := "select * from sample_07 limit 5;"
+	hSess, e := h.Exec(q, fnHR)
+	_ = hSess
+	_ = e
+}
+
+/*func TestHiveExecFile(t *testing.T) {
+	path := "/home/developer/hive.txt"
+	hSess, e := h.Exec(path, fnHR)
+}*/
