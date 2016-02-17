41565f31ad89c760eea9df07c1644ffb0cc40ebd
frezadev <freza.dev@gmail.com>
2016-02-17 11:13:29 +0700

    testing

--- terminal/main.go
+++ terminal/main.go
@@ -50,21 +50,21 @@ func (d *DuplexTerm) SendInput(input string) (result string, e error) {
 	return
 }
 
-func (d *DuplexTerm) Open() {
-	var err error
+func (d *DuplexTerm) Open() (err error) {
 	d.Cmd = exec.Command("sh", "-c", "beeline --outputFormat=csv2 -u jdbc:hive2://192.168.0.223:10000/default -n developer -d org.apache.hive.jdbc.HiveDriver")
 
 	d.Stdin, err = d.Cmd.StdinPipe()
-	check("stdin", err)
+	// check("stdin", err)
 
 	d.Stdout, err = d.Cmd.StdoutPipe()
-	check("stdout", err)
+	// check("stdout", err)
 
 	d.Writer = bufio.NewWriter(d.Stdin)
 	d.Reader = bufio.NewReader(d.Stdout)
 
 	err = d.Cmd.Start()
-	check("Start", err)
+	// check("Start", err)
+	return
 }
 
 func (d *DuplexTerm) Close() {
@@ -75,8 +75,7 @@ func (d *DuplexTerm) Close() {
 
 func main() {
 	dup := DuplexTerm{}
-	dup.Open()
-
+	err := dup.Open()
 	result, err := dup.SendInput("select * from sample_07 limit 5;")
 	fmt.Printf("error: %v\n", err)
 	result, err = dup.SendInput("!quit")
@@ -84,14 +83,14 @@ func main() {
 	_ = result
 	_ = err
 
-	/*for {
+	for {
 		bread, eread := dup.Reader.ReadString('\n')
 		if eread != nil && eread.Error() == "EOF" {
 			break
 		}
 		check("read", eread)
 		fmt.Println(bread)
-	}*/
+	}
 
 	defer dup.Close()
 	fmt.Println("Done")
