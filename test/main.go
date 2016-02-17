d01eb80424deed5d6970170583233fef319fc526
yanda15 <yanda151092@gmail.com>
2016-02-17 15:00:59 +0700

    detect data type parse out

--- test/main.go
+++ test/main.go
@@ -81,8 +81,14 @@ func TestParseOutput() {
 	h.ParseOutput(res, &tmp)
 	fmt.Println(tmp)
 
+	//try to parse json with different line
 	h.OutputType = "json"
-	res = "{ \"code\" : \"00-0000\" , \"description\" : \"All Occupations JSON\", \"total_emp\" : 134354, \"salary\" : 40690,\"Date\" : \"2012-04-23T18:25:43Z\" }"
+	res = "{ \"code\" : \"00-0000\" , \"description\" : \"All Occupations JSON\" "
+	tmp = SampleParse{}
+	h.ParseOutput(res, &tmp)
+	fmt.Println(tmp)
+
+	res = ", \"total_emp\" : 134354, \"salary\" : 40690,\"Date\" : \"2012-04-23T18:25:43Z\" }"
 	tmp = SampleParse{}
 	h.ParseOutput(res, &tmp)
 	fmt.Println(tmp)
