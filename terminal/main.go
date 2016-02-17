dc45ddcaa25803d8ab0a309acf6af44085cb4012
frezadev <freza.dev@gmail.com>
2016-02-17 10:27:50 +0700

    testing

--- terminal/main.go
+++ terminal/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	// "os"
+	"io"
 	"os/exec"
 	// "time"
 	// "runtime"
@@ -23,6 +24,8 @@ type DuplexTerm struct {
 	Writer *bufio.Writer
 	Reader *bufio.Reader
 	Cmd    *exec.Cmd
+	Stdin  io.WriteCloser
+	Stdout io.ReadCloser
 }
 
 func (d *DuplexTerm) SendInput(input string) (result string, e error) {
@@ -48,21 +51,19 @@ func (d *DuplexTerm) SendInput(input string) (result string, e error) {
 }
 
 func (d *DuplexTerm) Open() {
+	var err error
 	d.Cmd = exec.Command("sh", "-c", "beeline --outputFormat=csv2 -u jdbc:hive2://192.168.0.223:10000/default -n developer -d org.apache.hive.jdbc.HiveDriver")
 
-	stdin, err := d.Cmd.StdinPipe()
+	d.Stdin, err = d.Cmd.StdinPipe()
 	check("stdin", err)
-	//defer stdin.Close()
+	// defer d.Stdin.Close()
 
-	stdout, err := d.Cmd.StdoutPipe()
+	d.Stdout, err = d.Cmd.StdoutPipe()
 	check("stdout", err)
-	//defer stdout.Close()
+	// defer d.Stdout.Close()
 
-	bufin := bufio.NewWriter(stdin)
-	bufout := bufio.NewReader(stdout)
-
-	d.Writer = bufin
-	d.Reader = bufout
+	d.Writer = bufio.NewWriter(d.Stdin)
+	d.Reader = bufio.NewReader(d.Stdout)
 
 	err = d.Cmd.Start()
 	check("Start", err)
@@ -70,25 +71,20 @@ func (d *DuplexTerm) Open() {
 
 func (d *DuplexTerm) Close() {
 	d.Cmd.Wait()
+	d.Stdin.Close()
+	d.Stdout.Close()
 }
 
 func main() {
 	dup := DuplexTerm{}
 	dup.Open()
 
-	done := make(chan bool)
-
-	go func() {
-		result, err := dup.SendInput("select * from sample_07 limit 5;")
-		fmt.Printf("error: %v\n", err)
-		result, err = dup.SendInput("!quit")
-		fmt.Printf("error: %v\n", err)
-		_ = result
-		_ = err
-		done <- true
-	}()
-
-	<-done
+	result, err := dup.SendInput("select * from sample_07 limit 5;")
+	fmt.Printf("error: %v\n", err)
+	result, err = dup.SendInput("!quit")
+	fmt.Printf("error: %v\n", err)
+	_ = result
+	_ = err
 
 	for {
 		bread, eread := dup.Reader.ReadString('\n')
