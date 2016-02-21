f8731f91df25b34751423bc56fc7f547a9a4010a
RyanCi <ryan.angga.p@gmail.com>
2016-02-21 17:44:22 +0700

    test

--- hive/hive.go
+++ hive/hive.go
@@ -285,7 +285,7 @@ func (h *Hive) Load(TableName, Delimiter string, TableModel interface{}) (retVal
 
 		if v.Kind() == reflect.Struct {
 			for i := 0; i < v.NumField(); i++ {
-				if reflect.ValueOf(TableModel).Field(i) != nil {
+				if !reflect.ValueOf(TableModel).IsNil() {
 					if i == (v.NumField() - 1) {
 						insertValues += reflect.ValueOf(TableModel).Field(i).String() + ");"
 					} else {
