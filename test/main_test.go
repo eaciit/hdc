566cec7b688fc496529a24c885337f63e7f8c94b
Aris Meika <masmeka@gmail.com>
2016-02-22 19:27:42 +0700

    Modify main test file for hive to run LoadFileWithWorker

--- test/main_test.go
+++ test/main_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"github.com/eaciit/toolkit"
 	//. "github.com/frezadev/hdc/hive"
-	. "github.com/masmeka/hdc/hive"
+	. "github.com/eaciit/hdc/hive"
 	//. "github.com/RyanCi/hdc/hive"
 	"log"
 	"os"
