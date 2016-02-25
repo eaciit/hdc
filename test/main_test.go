a24b3f9449b40236e3408fb142f0b96f723ec331
RyanCi <ryan.angga.p@gmail.com>
2016-02-25 11:18:12 +0700

    update

--- test/main_test.go
+++ test/main_test.go
@@ -1,11 +1,11 @@
 package test
 
 import (
-	"github.com/eaciit/toolkit"
+	//"github.com/eaciit/toolkit"
 	//. "github.com/frezadev/hdc/hive"
 	//. "github.com/eaciit/hdc/hive"
 	. "github.com/RyanCi/hdc/hive"
-	"log"
+	//"log"
 	"os"
 	"testing"
 )
